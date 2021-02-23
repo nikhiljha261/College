1a00f8e5364bd0257e24051e6646289cb31e3a0d
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-02-23 19:27:25 +0530

    Add files via upload

---
+++ hw2.php
@@ -0,0 +1,25 @@
+<?php
+
+$osp = 4;
+$android = 4;
+$french = 2;
+$tarp=3;
+$soft_skill =1;
+$nescom=4;
+$urban=3;
+$hci=4;
+$total_credits = NULL;
+echo "osp : " . $osp .;
+echo "android : " . $android .;
+echo "french : " . $french .;
+echo "tarp : " . $tarp .;
+echo "soft_skill : " . $soft_skill .;
+echo "nescom : " . $nescom .;
+echo "urban : " . $urban .;
+echo "hci : " . $hci .;
+
+$total_credits = $osp + $android + $french + $soft_skill + $tarp + $nescom + $hci + $urban;
+
+echo "Total credits in winter semester 2021  = " . $total_credits . ;
+
+?>
